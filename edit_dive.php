d2d087295b3fbb74d752f77082ff8f6e0079eab2
sebastian <hi@sebastian-winkler.de>
2025-04-19 08:10:17 +0200

    Fixed bind_param mismatch error in edit_dive.php

--- edit_dive.php
+++ edit_dive.php
@@ -67,7 +67,7 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
                     water_type = ?
                     WHERE id = ?");
                 
-                $stmt->bind_param("sddssddddississiidsssi", 
+                $stmt->bind_param("sddssddddississdsssi", 
                     $_POST['location'],
                     $_POST['latitude'],
                     $_POST['longitude'],
